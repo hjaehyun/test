4246eaebfca5e0af060e35b9dd9f9fb0884cf160
hjaehyun <118723745+hjaehyun@users.noreply.github.com>
2023-10-30 20:37:44 +0900

    Create #calculrtor.py

---
+++ #calculrtor.py
@@ -0,0 +1,17 @@
+#calculrtor
+
+def plus(a,b):
+    return a + b
+
+def minus(a,b):
+    return a - b
+
+def multi(a.b):
+    return a * b
+
+def divi(a,b):
+    return a / b
+
+def sqr(a,b):
+    return a ** b
+
